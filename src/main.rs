11ca58516b11071b9c54ef8619c333614b654c85
Stephan Kern <stephan@swmmrman.com>
2025-06-29 20:13:20 -0600

    Read and dump serial port

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use serialport;
+use std::io::Read;
+use std::time::Duration;
+use std::thread::sleep;
+
 fn main() {
-    println!("Hello, world!");
+    let mut port = serialport::new("/dev/ttyACM0", 115200)
+        .timeout(Duration::from_millis(10))
+        .open().expect("failed to open port");
+    loop {
+        let mut serial_buff: Vec<u8> = vec![0; 256];
+        port.read(serial_buff.as_mut_slice()).expect("No data");
+        let text = String::from_utf8_lossy(&serial_buff);
+        println!("{}",text);
+        sleep(Duration::from_millis(2000));
+    }
 }
