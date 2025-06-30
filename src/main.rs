5e478b4bc0c1b4c877587db6e2e78f192b769e7f
Stephan Kern <stephan@swmmrman.com>
2025-06-29 20:29:04 -0600

    Error Handling

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use serialport;
-use std::io::Read;
+use std::io::{self,Read};
 use std::time::Duration;
 use std::thread::sleep;
 
@@ -7,11 +7,18 @@ fn main() {
     let mut port = serialport::new("/dev/ttyACM0", 115200)
         .timeout(Duration::from_millis(10))
         .open().expect("failed to open port");
+    let mut serial_buff: Vec<u8> = vec![0; 256];
     loop {
-        let mut serial_buff: Vec<u8> = vec![0; 256];
-        port.read(serial_buff.as_mut_slice()).expect("No data");
-        let text = String::from_utf8_lossy(&serial_buff);
-        println!("{}",text);
-        sleep(Duration::from_millis(2000));
+        match port.read(serial_buff.as_mut_slice()) {
+            Ok(_) => {
+                let text = String::from_utf8_lossy(&serial_buff);
+                println!("{}",text);
+            },
+            //From the examples..  Do nothing if timed out.
+            Err(ref e) if e.kind() == io::ErrorKind::TimedOut => (),
+            //Print error otherwise
+            Err(e) => eprintln!("{:?}", e),
+        }
+        sleep(Duration::from_millis(100));
     }
 }
