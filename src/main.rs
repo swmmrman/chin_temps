e21a9a54a4a7e651561fd088b5d25dc93cbfe83c
Stephan Kern <stephan@swmmrman.com>
2025-06-29 21:38:57 -0600

    Create Struct to hold evap data

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,16 @@ use std::io::{self,Read};
 use std::time::Duration;
 use std::thread::sleep;
 
+struct EvapData {
+    temp1: f32,
+    temp2: f32,
+    temp3: f32,
+    humid1: f32,    
+    humid2: f32,
+    humid3: f32,
+    valve_status: i8,    
+}
+
 fn main() {
     let mut port = serialport::new("/dev/ttyACM0", 115200)
         .timeout(Duration::from_millis(10))
