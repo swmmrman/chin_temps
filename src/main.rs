17b2422547974ec490f750e3fbb9bcf37cb4b93d
Stephan Kern <stephan@swmmrman.com>
2025-07-02 06:44:55 -0600

    Trying to filter off incomplete results.

--- src/main.rs
+++ src/main.rs
@@ -16,9 +16,12 @@ struct Temp {
 
 impl Temp {
     fn update(&mut self, new_temp: f32) {
-        self.cur_temp = new_temp;
+        //filter off errors on the first temp
+        let temp_diff = self.cur_temp - &new_temp;
+        if temp_diff.abs() < 10.0f32 {self.cur_temp = new_temp;}
         //Clear NaNs first
-        if self.min_temp.is_nan() { self.min_temp = new_temp };
+        if self.cur_temp.is_nan() { self.cur_temp = new_temp }
+        if self.min_temp.is_nan() { self.min_temp = new_temp }
         if self.max_temp.is_nan() { self.max_temp = new_temp }
         //Then check min/max
         if self.cur_temp < self.min_temp { self.min_temp = new_temp; }
