1793f1b55f84a3e18a079355f0cb5f01231928ae
Stephan Kern <stephan@swmmrman.com>
2025-07-02 19:11:42 -0600

    Early Skeleton

--- src/serial_parser/main.rs
+++ src/serial_parser/main.rs
@@ -1,3 +1,21 @@
-pub mod reader{
+pub mod parser{
+    struct parsed_data{
+        partial: String,
+        completed: String,
+    }
+    pub fn add_and_return(input: Vec<u8>) -> Vec<String> {
 
+    }
+    pub fn new() -> parser {
+
+    }
+    fn convert_add(&mut self) {
+
+    }
+    fn get_completed(&self) -> String {
+
+    }
+    fn clear_completed(&mut self) {
+        
+    }
 }
