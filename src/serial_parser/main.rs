044be2f698a97d29317dc59f4213c1e5dc74a585
Stephan Kern <stephan@swmmrman.com>
2025-07-02 19:22:18 -0600

    Code from covert and add

--- src/serial_parser/main.rs
+++ src/serial_parser/main.rs
@@ -15,8 +15,12 @@ pub mod serail_parser{
         pub fn add_and_return(input: Vec<u8>) -> Vec<String> {
 
         }
-        fn convert_add(&mut self) {
-
+        fn convert_add(&mut self, incoming_vec: &Vec<u8>) {
+            let outstring = String::new();
+            for byte in incoming_vec {
+                outstring.push(*byte as char);
+            }
+            &self.partial.partial.push_str(outstring);
         }
         fn get_completed(&self) -> String {
 
