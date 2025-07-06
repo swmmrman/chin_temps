86f9c986bb6ef9a011f15fdcf6a861a7463ac4fc
Stephan Kern <stephan@swmmrman.com>
2025-07-06 03:42:39 -0600

    Get reading function

--- src/sensors.rs
+++ src/sensors.rs
@@ -24,7 +24,22 @@ impl sensor {
     fn get_all(&self) {
 
     }
-    fn get_reading(&self,  reading: r_type, kind: r_kind) {
-        
+    fn get_reading(&self,  reading: r_type, kind: r_kind) -> f32 {
+        match reading {
+            self::r_type::temp => {
+                match kind {
+                    self::r_kind::min => self.temperature.get_min(),
+                    self::r_kind::max => self.temperature.get_max(),
+                    self::r_kind::cur => self.temperature.get_cur()
+                }
+            }
+            self::r_type::humidity => {
+                match kind {
+                    self::r_kind::min => self.humidity.get_min(),
+                    self::r_kind::max => self.humidity.get_max(),
+                    self::r_kind::cur => self.humidity.get_cur(),
+                }
+            }
+        }
     }
 }
