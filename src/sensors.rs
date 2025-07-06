14547f4ec5d9558df23acbece39cbd4693da2c06
Stephan Kern <stephan@swmmrman.com>
2025-07-06 03:32:26 -0600

    Skeletons

--- src/sensors.rs
+++ src/sensors.rs
@@ -6,3 +6,25 @@ struct sensor {
     temperature: temp::Temp,
     humidity: rh::RH
 }
+struct readings {
+
+}
+
+enum r_type{
+    temp,
+    humidity,
+}
+
+enum r_kind {
+    min,
+    max,
+    cur
+}
+impl sensor {
+    fn get_all(&self) {
+
+    }
+    fn get_reading(&self,  reading: r_type, kind: r_kind) {
+        
+    }
+}
