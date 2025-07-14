f02833cd54a6185e93a3d2e820194e857c85156c
Stephan Kern <stephan@swmmrman.com>
2025-07-14 00:51:27 -0600

    Quick check for a second sensor

--- src/Arduino_Code/Test_HDC302x/Test_HDC302x.ino
+++ src/Arduino_Code/Test_HDC302x/Test_HDC302x.ino
@@ -1,16 +1,23 @@
 #include <Adafruit_HDC302x.h>
+#include <LiquidCrystal_I2C.h>
+
 
 Adafruit_HDC302x sense1 = Adafruit_HDC302x();
+Adafruit_HDC302x sense2 = Adafruit_HDC302x();
+LiquidCrystal_I2C lcd(0x27, 20, 4);
 
 void setup() {
   Serial.begin(115200);
   if(!sense1.begin(0x44, &Wire)) {
-    Serial.println("No sensor");
+    Serial.println("No sensor 1");
     while(1) {
       if(!sense1.begin(0x44, &Wire)) {
         break;
       }
     }
+  }
+  if(!sense2.begin(0x45, &Wire)) {
+    Serial.println("No sensor 2");
   }
 }
 
