f915b304b20c1cc8545d9f34fa5f56a2c3ba6f34
Stephan Kern <stephan@swmmrman.com>
2025-07-14 00:39:56 -0600

    Quick Single sensor test code

--- src/Arduino_Code/Test_HDC302x/Test_HDC302x.ino
+++ src/Arduino_Code/Test_HDC302x/Test_HDC302x.ino
@@ -15,5 +15,13 @@ void setup() {
 }
 
 void loop() {
-  
+  double temp = 0.0;
+  double RH = 0.0;
+
+  sense1.readTemperatureHumidityOnDemand(temp, RH, TRIGGERMODE_LP0);
+  Serial.print("Temp: ");
+  Serial.print(temp);
+  Serial.print("f.  RH: ");
+  Serial.println(RH);
+  delay(1000);
 }
