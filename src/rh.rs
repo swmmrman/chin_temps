d21fa7dd4801b3c8c533522b9e8d5effb4e9aa5d
Stephan Kern <stephan@swmmrman.com>
2025-07-28 01:30:08 -0600

    Fix comments so they display in IDEs

--- src/rh.rs
+++ src/rh.rs
@@ -6,27 +6,27 @@ pub mod rh {
     }
 
     impl RH {
-        //Clears the RH struct, setting all values to NAN.
+        /// Clears the RH struct, setting all values to NAN.
         pub fn clear(&mut self) {
             self.min_rh = f32::NAN;
             self.max_rh = f32::NAN;
             self.cur_rh = f32::NAN;
         }
-        //Returns the current RH.  NAN if not set
+        /// Returns the current RH.  NAN if not set
         pub fn get_cur(&self) -> f32 {
             self.cur_rh
         }
-        //Returns the min RH.  NAN if unset.
+        /// Returns the min RH.  NAN if unset.
         pub fn get_min(&self) -> f32 {
             self.min_rh
         }
-        //Returns the max RH. NAN if unset.
+        /// Returns the max RH. NAN if unset.
         pub fn get_max(&self) -> f32 {
             self.max_rh
         }
-        //Checked update of the RH struct.
-        //Sets all values to the new if NAN in min_rh
-        //Updates min and max as needed.
+        /// Checked update of the RH struct.
+        /// Sets all values to the new if NAN in min_rh
+        /// Updates min and max as needed.
         pub fn update(&mut self, new_val: f32) {
             let rh_diff= (self.cur_rh - new_val).abs();
             if self.min_rh.is_nan() {
@@ -41,14 +41,14 @@ pub mod rh {
             }
             self.cur_rh = new_val;
         }
-        //Sets the min, max, and current RH.  No checking or validation is performed.
+        /// Sets the min, max, and current RH.  No checking or validation is performed.
         pub fn update_unchecked(&mut self, min: f32, max: f32, cur: f32) {
             self.min_rh = min;
             self.max_rh = max;
             self.cur_rh = cur;
         }
     }
-    //Create a new RH stuct with NAN values.
+    /// Create a new RH stuct with NAN values.
     pub fn new() -> RH{
         RH {
             min_rh: f32::NAN,
