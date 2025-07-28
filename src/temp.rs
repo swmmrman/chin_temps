b4524daf226a55a50a5f8535559f4f6f5bbdcaa4
Stephan Kern <stephan@swmmrman.com>
2025-07-28 16:00:12 -0600

    Documentation

--- src/temp.rs
+++ src/temp.rs
@@ -6,40 +6,40 @@ pub mod temp {
     }
 
     impl Temp {
-        //Updates the temp struct,  Checks for large jumps due to sensor glitch.
-        //Sets all values if currently NANs.
-        //Sets min/max as needed.
-        //Current temp is always updated incase of temporary sensor failure.
+        /// Updates the temp struct,  Checks for large jumps due to sensor glitch.
+        /// Sets all values if currently NANs.
+        /// Sets min/max as needed.
+        /// Current temp is always updated incase of temporary sensor failure.
         pub fn update(&mut self, new_temp: f32) {
             let temp_diff = (self.cur_temp - &new_temp).abs() < 2.0;
             self.cur_temp = new_temp;
-            //Clear NaNs first
+            // Clear NaNs first
             if self.cur_temp.is_nan() && temp_diff { self.cur_temp = new_temp }
             if self.min_temp.is_nan() && temp_diff { self.min_temp = new_temp }
             if self.max_temp.is_nan() && temp_diff { self.max_temp = new_temp }
-            //Then check min/max
+            // Then check min/max
             if self.cur_temp < self.min_temp { self.min_temp = new_temp; }
             if self.cur_temp > self.max_temp { self.max_temp = new_temp; }
         }
-        //Sets the min, max, and current temp.  No checking or validation is performed.
+        /// Sets the min, max, and current temp.  No checking or validation is performed.
         pub fn update_unchecked(&mut self, min: f32, max: f32, cur: f32) {
             self.min_temp = min;
             self.max_temp = max;
             self.cur_temp = cur;
         }
-        //Returns the current temp.  NAN if not set
+        /// Returns the current temp.  NAN if not set
         pub fn get_cur(&self) -> f32 {
             self.cur_temp
         }
-        //Returns the current temp.  NAN if not set
+        /// Returns the current temp.  NAN if not set
         pub fn get_min(&self) -> f32 {
             self.min_temp
         }
-        //Returns the current temp.  NAN if not set
+        /// Returns the current temp.  NAN if not set
         pub fn get_max(&self) -> f32 {
             self.max_temp
         }
-        //Clears the temp strut,  Sets all values to NAN.
+        /// Clears the temp strut,  Sets all values to NAN.
         pub fn clear(&mut self) {
             self.cur_temp = f32::NAN;
             self.min_temp = f32::NAN;
