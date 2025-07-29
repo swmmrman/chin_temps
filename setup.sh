6dfb4a5c132999d3f88242157eaa222a95680d5a
Stephan Kern <stephan@swmmrman.com>
2025-07-29 02:51:36 -0600

    Quick setup script, and config.ron file

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+if [ $(id -u) -ne 0 ]
+    then echo "This script run this as root."
+    exit
+fi
+
+if [ ! -e /etc/chin_temps/]
+    then
+    mkdir /etc/chin_temps/
+fi
+cp config.ron /etc/chin_temps/config.ron
