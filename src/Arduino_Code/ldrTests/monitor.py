3862b6c2b9a771d8fae1bfa163c7e21030d8bf1e
Stephan Kern <stephan@swmmrman.com>
2026-05-12 19:46:44 -0600

    Stray ,

--- src/Arduino_Code/ldrTests/monitor.py
+++ src/Arduino_Code/ldrTests/monitor.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 import serial
 
-s = serial.Serial("/dev/ttyUSB0,", 115200, timeout=1)
+s = serial.Serial("/dev/ttyUSB0", 115200, timeout=1)
 try:
     while True:
         ser_in = s.readline().decode("utf-8").strip("\n")
