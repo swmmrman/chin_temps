e58e5d591777cf546925175449f4bf9defa10812
Stephan Kern <stephan@swmmrman.com>
2026-05-14 17:10:18 -0600

    Update paths

--- src/Arduino_Code/flash.sh
+++ src/Arduino_Code/flash.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ $# -ne 2 ]; then
-    arduino-cli compile -b arduino:avr:mega Water_Controller_DHT22.ino
-    arduino-cli upload -b arduino:avr:mega -p /dev/ttyUSB0 Water_Controller_DHT22.ino
+    arduino-cli compile -b arduino:avr:mega Water_Controller_DHT22/Water_Controller_DHT22.ino
+    arduino-cli upload -b arduino:avr:mega -p /dev/ttyUSB0 Water_Controller_DHT22/Water_Controller_DHT22.ino
 else
-    arduino-cli compile -b arduino:avr:mega Water_Controller.ino
-    arduino-cli upload -b arduino:avr:mega -p /dev/ttyUSB0 Water_Controller.ino
+    arduino-cli compile -b arduino:avr:mega Water_Controller_DHT22/Water_Controller.ino
+    arduino-cli upload -b arduino:avr:mega -p /dev/ttyUSB0 Water_Controller_DHT22/Water_Controller.ino
 fi
