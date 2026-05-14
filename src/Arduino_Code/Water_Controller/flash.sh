026ef38441be64d411e7b134afc0c0b4e2264f8d
Stephan Kern <stephan@swmmrman.com>
2026-05-14 17:07:17 -0600

    Script to compile the arduino firmware

--- src/Arduino_Code/Water_Controller/flash.sh
+++ src/Arduino_Code/Water_Controller/flash.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-arduino-cli compile -b arduino:avr:mega ldrTests.ino
-arduino-cli upload -b arduino:avr:mega -p /dev/ttyUSB0 ldrTests.ino
+if [ $# -ne 2 ]; then
+    arduino-cli compile -b arduino:avr:mega Water_Controller_DHT22.ino
+    arduino-cli upload -b arduino:avr:mega -p /dev/ttyUSB0 Water_Controller_DHT22.ino
+else
+    arduino-cli compile -b arduino:avr:mega Water_Controller.ino
+    arduino-cli upload -b arduino:avr:mega -p /dev/ttyUSB0 Water_Controller.ino
+fi
