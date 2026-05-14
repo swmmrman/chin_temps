ea9b0a42d322b9fe568c896a067a4dda77a5d9b2
Stephan Kern <stephan@swmmrman.com>
2026-05-14 17:58:38 -0600

    Fix flash script

--- src/Arduino_Code/flash.sh
+++ src/Arduino_Code/flash.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-    arduino-cli compile -b arduino:avr:mega Water_Controller_DHT22/Water_Controller_DHT22.ino
-    arduino-cli upload -b arduino:avr:mega -p /dev/ttyUSB0 Water_Controller_DHT22/Water_Controller_DHT22.ino
+if [ $# -eq 2 ]; then
+    echo "Compiling for NANO and DHT22"
+    arduino-cli compile -b arduino:avr:nano Water_Controller_DHT22/Water_Controller_DHT22.ino
+    arduino-cli upload -b arduino:avr:nano -p /dev/ttyUSB0 Water_Controller_DHT22/Water_Controller_DHT22.ino
+
 else
-    arduino-cli compile -b arduino:avr:mega Water_Controller_DHT22/Water_Controller.ino
-    arduino-cli upload -b arduino:avr:mega -p /dev/ttyUSB0 Water_Controller_DHT22/Water_Controller.ino
+    echo "Compiling for mega with HDC3022"
+    arduino-cli compile -b arduino:avr:mega Water_Controller/Water_Controller.ino
+    arduino-cli upload -b arduino:avr:mega -p /dev/ttyUSB0 Water_Controller/Water_Controller.ino
 fi
