08769fc44a9fba93ea498326210e88e784613b70
Stephan Kern <stephan@swmmrman.com>
2026-05-14 19:20:03 -0600

    Help message

--- src/Arduino_Code/flash.sh
+++ src/Arduino_Code/flash.sh
@@ -2,11 +2,23 @@
 
 elf=$0
 print_usage() {
-    echo "Usage: $elf board-type sensor type port"
+    echo "Usage: $elf board-type sensor-type port"
+}
+
+print_help(){
+    print_usage
+    echo -e "
+  Currently supported boards are mega and uno
+    Sensor can be DHT22 or HDC for HDC302x
+    Port will either be
+      /dev/ttyUSBx for some mega boards or
+      /dev/ttyACMx for most other arduino
+      x is 0 usualy if one adruino is connected.
+"
 }
 
 port="/dev/ttyUSB0"
-if [ $# -eq 2 ]; then
+if [ $# -gt 1 ]; then
     if [ "$1" = "nano" ]; then
         fqbn="arduino:avr:nano"
         board="Arduino Nano"
@@ -27,6 +39,8 @@ if [ $# -eq 2 ]; then
     echo "Compiling for $1 with $sensor on $port"
     arduino-cli compile -b $fqbn $target/$target.ino
     arduino-cli upload -b $fqbn -p $port $target/$target.ino
+elif [ "$1" = "help" ]; then
+    print_help
 else
     print_usage
     exit 1
