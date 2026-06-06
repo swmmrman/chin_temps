4facfce590c1274cf191409e65f1a65e3369c385
Stephan Kern <stephan@swmmrman.com>
2026-06-06 02:43:57 -0600

    Look for -1 on fan call

--- src/evap_data.rs
+++ src/evap_data.rs
@@ -184,7 +184,7 @@ Min%{: >6.2} Max %{: >6.2} LDR: {}\t\tMin:  {: >7.2}f   Max: {: >7.2}f",
         ) {
             let ts = Local::now().timestamp();
             if call == "on" {
-                if self.fan_call == 0 {
+                if self.fan_call <= 0 {
                     self.delay_start = ts;
                     self.set_water_call(sp, 1, update);
                     self.fan_call = 2;
