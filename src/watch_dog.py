1219aabcc23e0c7e83574770461e39c58aca367a
Stephan Kern <stephan@swmmrman.com>
2026-06-06 17:36:06 -0600

    Imports

--- src/watch_dog.py
+++ src/watch_dog.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
-import sys
-
 ## A simple watch dog script to use the RPi to reset a locked up arduino.
+import sys
+from time import sleep
+
+import RPi.GPIO as GPIO
+
 if __name__ != "__main__":
     print("Not a library,  Do not import")
     sys.exit(1)
 
 reset_pin = 26
+reset_file = "/tmp/page/reset_arduino"
 
 
 def reset_arduino():
