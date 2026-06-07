ab4c4cce2da88d4c6c43adcdfa9c6ce0385f5231
Stephan Kern <stephan@swmmrman.com>
2026-06-07 15:51:45 -0600

    Auto format and add LogFiles struct

--- src/logging.rs
+++ src/logging.rs
@@ -1,31 +1,29 @@
 pub mod logging {
-    use std::fs::{self,File};
-    use std::path;
-    use std::io::Write;
     use crate::EvapData;
+    use std::fs::{self, File};
+    use std::io::Write;
+    use std::path;
 
     use chrono::{DateTime, Local};
 
     pub fn make_log_file() -> File {
         let home_dir = std::env::home_dir().unwrap();
         let logs_path = home_dir.join("logs/evap/");
-        if ! path::Path::exists(&logs_path) {
+        if !path::Path::exists(&logs_path) {
             fs::create_dir_all(&logs_path).unwrap()
         }
         let log_file = fs::OpenOptions::new()
             .create(true)
             .write(true)
             .append(true)
-            .open(path::Path::new(&logs_path).join("evap.log")).unwrap();
+            .open(path::Path::new(&logs_path).join("evap.log"))
+            .unwrap();
         log_file
     }
 
-    pub fn write_to_log(
-            short_term: &mut EvapData,
-            new_date: DateTime<Local>,
-            log_file: &mut File,
-        ) {
-        let log_string = format!("\n[{}]\n{}\n", 
+    pub fn write_to_log(short_term: &mut EvapData, new_date: DateTime<Local>, log_file: &mut File) {
+        let log_string = format!(
+            "\n[{}]\n{}\n",
             new_date.format("%m-%d-%Y %H:%M:%S"),
             short_term.get_evap_data(),
         );
@@ -36,5 +34,11 @@ pub mod logging {
             }
         }
         short_term.clear();
+    }
+
+    pub struct Logfiles {
+        error_log: File,
+        history_log: File,
+        adjustments_log: File,
     }
 }
