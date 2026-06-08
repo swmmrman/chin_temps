0f83710e4b78430bdf6cc4be06e954027747b859
Stephan Kern <stephan@swmmrman.com>
2026-06-07 21:26:46 -0600

    make log fies and directory if needed

--- setup.sh
+++ setup.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
-if [ $(id -u) -ne 0 ]
-    then echo "This script run this as root."
+if [ $(id -u) -ne 0 ]; then
+    echo "This script run this as root."
     exit
 fi
 
-if [ ! -e /etc/chin_temps/ ]
-    then
+if [ ! -e /etc/chin_temps/ ]; then
     mkdir /etc/chin_temps/
 fi
 cp config.ron /etc/chin_temps/config.ron
+
+if [ ! -e /var/log/evap/ ]; then
+    mkdir -p /var/log/evap/
+    touch /var/log/evap/error.log
+    touch /var/log/evap/history.log
+    touch /var/log/evep/adjustments.log
+fi
