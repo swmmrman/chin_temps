8e6e10d754323cd964209d0af760c7f1894cf3c0
Stephan Kern <stephan@swmmrman.com>
2026-06-28 21:26:53 -0600

    handle no args

--- src/evap_configure_tool.rs
+++ src/evap_configure_tool.rs
@@ -1,3 +1,11 @@
 fn main() {
     let args = std::env::args();
+    if args.len() < 2 {
+        println!("No args supplied");
+        print_help();
+        std::process::exit(1);
+    }
+    for arg in args {
+        println!("{}", arg);
+    }
 }
