0cb9f1131b20b2da40069012a4392227065bbdab
Stephan Kern <stephan@swmmrman.com>
2026-06-28 21:34:33 -0600

    Help message

--- src/evap_configure_tool.rs
+++ src/evap_configure_tool.rs
@@ -1,8 +1,21 @@
+fn print_help(call: String) {
+    println!(
+        "
+Usage: {} command value
+
+Commands:
+  tsettemp\tvalue\tSet the temperature setpoint
+  sethumid\tvalue\tSet the humidity target
+        ",
+        call
+    )
+}
+
 fn main() {
-    let args = std::env::args();
+    let mut args = std::env::args();
     if args.len() < 2 {
         println!("No args supplied");
-        print_help();
+        print_help(args.nth(0).unwrap());
         std::process::exit(1);
     }
     for arg in args {
